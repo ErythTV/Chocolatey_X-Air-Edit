47ba7bcee6d0f4feb58e9d10b11762197e251073
ErythTV <91645936+ErythTV@users.noreply.github.com>
2021-09-30 04:56:01 +0200

    asd

---
+++ X-Air-Edit/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"  
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+  url           = 'https://mediadl.musictribe.com/download/software/behringer/XAIR/X-AIR-Edit_PC_1.7.zip'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Install start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs 'X-AIR-Edit.lnk'
+$targetPath = Join-Path $toolsDir 'X-AIR-Edit.exe'
+Install-ChocolateyShortcut -shortcutFilePath "$shortcutFilePath" -targetPath "$targetPath"
---
+++ X-Air-Edit/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿# Remove start menu shortcut
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs 'X-AIR-Edit.lnk'
+If ( Test-Path "$shortcutFilePath" ) { Remove-Item "$shortcutFilePath" }
